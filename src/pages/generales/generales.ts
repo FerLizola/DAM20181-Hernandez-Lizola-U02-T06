6c5e272ce83112130be7a657058f9048b0f1374b
FerLizola <ferlizola95@gmail.com>
2018-04-18 08:33:25 -0600

    Initial commit

---
+++ src/pages/generales/generales.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'generales',
+  templateUrl: 'generales.html'
+})
+export class Generales {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
